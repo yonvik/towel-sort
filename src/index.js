1a39fa0b40c275db0c8ccebd19428a2940404ded
Andrey Yankovskiy <tiamoonrock@gmail.com>
2020-10-06 21:45:17 +0300

    feat: task remove

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  for (let i = 0; i < matrix.length; i++){
-    for (let k = 0; k < matrix[i].length; k++) {
-      return (matrix [i][k])
+  //for (let i = 0; i < matrix.length; i++){
+  //  
+  //  let train = [];
+//
+  //  for (let k = 0; k < matrix[i].length; k++) {
+  //   train[i].push(k);
+  //   return (train);
+   // } 
+  //    } 
+
+
+
+//let newArr = []
+//
+//let i = 0;
+//for (subArray of matrix) {
+//  if ( i % 2 === 0) {
+//    newArr = [...newArr, ...subArray]
+//  } else {
+//    newArr = [...newArr, ...subArray.reverse()]
+//  }
+//  i++
+//  };
+//  newArr.forEach( e => console.log(e))
+//  return (newArr)
+
+
+
+  
+  for(let i = 0; i < matrix.length; i++){
+    if(i % 2) { 
+      matrix[i].reverse(); 
     }
-      }
-  }
+}
+return matrix.reduce((acc, subArray) => acc.concat(subArray), [])
+} 
+
+  
